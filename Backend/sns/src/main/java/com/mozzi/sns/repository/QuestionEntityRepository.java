8cab8f715d1251bb94a04dc090cf972367686685
beomchul.kim <2017253083@yonsei.ac.kr>
2023-01-15 18:42:25 +0900

    [BE][수정] 임포트정리

--- Backend/sns/src/main/java/com/mozzi/sns/repository/QuestionEntityRepository.java
+++ Backend/sns/src/main/java/com/mozzi/sns/repository/QuestionEntityRepository.java
@@ -1,10 +1,6 @@
 package com.mozzi.sns.repository;
 
-import com.mozzi.sns.domain.entity.PostEntity;
 import com.mozzi.sns.domain.entity.QuestionEntity;
-import com.mozzi.sns.domain.entity.UserEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 

ef14c16a2bc8bbe6e5c8f50fbfacb39afb230353
joohwan812 <joohon812@nate.com>
2023-01-17 03:30:45 +0900

    [FE][수정] 라우터 : 컴포넌트와 url 매핑

--- Frontend/sns/src/App.svelte
+++ Frontend/sns/src/App.svelte
@@ -1,9 +1,18 @@
 <script>
-  import Router from 'svelte-spa-router'
-  import Board from './routes/Board.svelte'
+    import Router from 'svelte-spa-router'
+    import Modal from './Common/Modal.svelte'
+    import Board from './routes/Board.svelte'
+    import Contact from './routes/Contact.svelte'
+    import Join from './routes/Join.svelte'
+    import Login from './routes/Login.svelte'
+    import Test from './routes/Test.svelte'
 </script>
 
 <Router routes = {{
-	'/': Board,
-	'/board': Board
+    '/': Test,
+    '/login': Login,
+    '/join': Join,
+    '/contact': Contact,
+    '/board': Board,
 }} />
+

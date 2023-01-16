7c7901bbc7333b0368641457a405cf53acfa2eb2
joohwan812 <joohon812@nate.com>
2023-01-17 03:34:41 +0900

    [FE][추가] 퍼블리싱 - 공통 페이지 진행중 TODO - Alert, Confirm 공통화 - 모달 바깥 영역 클릭 시, 모달 닫기

---
+++ Frontend/sns/src/Common/Modal.svelte
@@ -0,0 +1,63 @@
+<script>
+	export let text = '성공했습니다.'
+    import { createEventDispatcher, onDestroy } from 'svelte';
+
+	const dispatch = createEventDispatcher();
+	const close = () => dispatch('close');
+</script>
+
+<div class="modal-container-wrapper">
+    <div class="modal-container">
+        <div class="modal-content">
+            <div>😎</div>
+            <div>{text}</div>
+        </div>
+        <button class="modal-footer" on:click={() => close()}>
+            확인
+        </button>
+    </div>
+    
+    
+</div>
+
+<style lang="scss">
+    .modal-container-wrapper {
+        position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		background: rgba(0,0,0,0.3);
+
+        .modal-container {
+            border-radius: 20px 20px 0px 0px;
+            height: 400px;
+            position: absolute;
+            bottom: 0px;
+            width: 100%;
+            background-color: #FFFFFF;
+    
+            .modal-content {
+                height: 360px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+                font-weight: 700;
+            }
+            
+    
+            .modal-footer {
+                height: 43px;
+                background-color: #377375;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                font-weight: 700;
+                font-size: 14px;
+                color: #FFFFFF;
+                width: 100%;
+            }
+        }
+    }
+</style>

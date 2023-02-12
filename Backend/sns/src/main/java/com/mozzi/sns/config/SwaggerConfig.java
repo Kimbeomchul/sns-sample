aa5ffae4ac1c24df593ae62033e274a3a03e185a
beomchul.kim <2017253083@yonsei.ac.kr>
2023-02-12 21:47:49 +0900

    [BE] Swagger JWT Authorize 공통버튼추가
    Bearer {token}

--- Backend/sns/src/main/java/com/mozzi/sns/config/SwaggerConfig.java
+++ Backend/sns/src/main/java/com/mozzi/sns/config/SwaggerConfig.java
@@ -1,14 +1,19 @@
 package com.mozzi.sns.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * packageName : com.mozzi.sns.config
@@ -20,8 +25,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  * DATE    AUTHOR    NOTE
  * -----------------------------------------------------------
  * 2023/01/15 kimbeomchul 최초 생성
+ * 2023/02/12 kimbeomchul JWT토큰 헤더 추가
  */
 
+@Slf4j
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
@@ -33,14 +40,40 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.mozzi.sns"))
                 .paths(PathSelectors.any())
-                .build();
+                .build()
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
     }
 
     public ApiInfo apiInfo(){
         return new ApiInfoBuilder()
                 .title("GwacheonE Project")
                 .version("1.0.0")
+                .contact(new Contact("Beomchul Kim", "https://www.github.com/Kimbeomchul", "2017253083@yonsei.ac.kr"))
                 .description("과처니 프로젝트")
+                .license("Apache License Version 2.0")
                 .build();
+    }
+
+    private SecurityContext securityContext() {
+        return springfox
+                .documentation
+                .spi.service
+                .contexts
+                .SecurityContext
+                .builder()
+                .securityReferences(defaultAuth()).forPaths(PathSelectors.any()).build();
+    }
+
+    // Authorization : Bearer {token} 헤더추가
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Authorization", "header");
+    }
+
+    List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("JWT", authorizationScopes));
     }
 }
